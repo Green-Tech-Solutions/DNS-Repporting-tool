8679bb58c4881bcdc8137db9bf4c68daddcc8ce7
Jason Kahn <jjjkahn@gmail.com>
2023-12-30 18:21:05 -0500

    Configuration Services added

---
+++ DNS.Repporting.Tool.Core/Configurations/ConfigurationService.cs
@@ -0,0 +1,21 @@
+﻿using DNS.Repporting.Tool.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace DNS.Repporting.Tool.Core;
+
+public class ConfigurationService : IConfigurationService
+{
+    private readonly ILogger<RapportService> _log;
+    private readonly IConfiguration _config;
+
+    public ConfigurationService(ILogger<RapportService> log, IConfiguration config)
+    {
+        _log = log;
+        _config = config;
+    }
+
+    public void SetupConfigurations()
+    {
+    }
+}
---
+++ DNS.Repporting.Tool.Models/ApiInformation.cs
@@ -0,0 +1,8 @@
+﻿namespace DNS.Repporting.Tool.Models;
+
+public class ApiInformation
+{
+    public string Endpoint { get; set; } = String.Empty;
+    public string Profiles { get; set; } = string.Empty;
+    public string Logs { get; set; } = string.Empty;
+}
