5b6a9940761cca120374c11c8f2820be1e97eecf
Jason Kahn <jjjkahn@gmail.com>
2023-12-29 19:35:27 -0500

    Added the RepportService Interface

--- DNS.Repporting.Tool.Core/Program.cs
+++ DNS.Repporting.Tool.Core/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DNS.Repporting.Tool.Core
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DNS.Repporting.Tool.Core
 {
     public class Program
     {
@@ -6,6 +8,12 @@
         {
             Console.WriteLine("Hello, World!");
             Console.ReadLine();
+        }
+
+        static void BuildConfig(IConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json",optional:false,)
         }
     }
 }
---
+++ DNS.Repporting.Tool.Core/RapportService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNS.Repporting.Tool.Core
+{
+    public class RapportService
+    {
+        private readonly ILogger<RapportService> _log;
+        private readonly IConfiguration _config;
+
+        public RapportService(ILogger<RapportService> log, IConfiguration config)
+        {
+            _log = log;
+            _config = config;
+        }
+
+        public void CreateRapport()
+        {
+            _log.LogInformation("Creating rapport");
+            Console.ReadLine();
+        }
+    }
+}
