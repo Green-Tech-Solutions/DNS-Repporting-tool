549d2d2a4069ff975cfd215a1e93fad3a106b657
jjjkahn <31192156+jjjkahn@users.noreply.github.com>
2024-01-05 22:20:28 -0500

    Fixed the jobs (#7)
    they are not present

--- .github/workflows/my-script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-ls
-cd DNS.Repporting.Tool.Core
-dotnet restore
-echo script ran this time
--- .github/workflows/restoreScript.sh
+++ .github/workflows/restoreScript.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-ls
 cd DNS.Repporting.Tool.Core
 dotnet restore
-echo script ran this time
